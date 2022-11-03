d6d8a15a60e2c09db512c6bacd9b35929aecebc9
Faizan Ahmed <53269044+faizywaizy@users.noreply.github.com>
2022-11-03 15:50:03 -0400

    maps create

--- src/components/MapsPage.jsx
+++ src/components/MapsPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
+
 function MapsPage() {
   return (
         <div class="vh-100 gradient-custom container-fluid vh-80 py-1">
           <h1> Maps Page</h1>
-
         </div> 
   );
 }
