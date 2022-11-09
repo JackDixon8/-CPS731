357d4d291b50d41949a00cd7ab1a3e808f323185
drc-333 <d1chong@ryerson.ca>
2022-11-08 23:11:43 -0500

    finished checkout page

--- src/components/MapsPage.jsx
+++ src/components/MapsPage.jsx
@@ -4,8 +4,7 @@ import React from "react";
 function MapsPage() {
   return (
         <div class="vh-100 gradient-custom container-fluid vh-80 py-1">
-          <h1> Maps Page</h1>
-
+          <h1> Maps Page</h1> 
         </div> 
   );
 }
