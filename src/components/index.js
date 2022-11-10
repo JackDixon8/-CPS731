65066cbbde7a644386b39e229a5035a148e5755c
Faizan Ahmed <53269044+faizywaizy@users.noreply.github.com>
2022-11-09 22:11:51 -0500

    update index checkout merge conflicts

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Login } from "./Login";
 export { default as MapsPage } from "./MapsPage";
 export { default as OrderDetails } from "./OrderDetails";
 export { default as CreateAccount } from "./CreateAccount";
+export { default as Checkout } from "./Checkout";
