f389454fde9711d9f4446dcc858e1a702497c23d
Faizan Ahmed <53269044+faizywaizy@users.noreply.github.com>
2022-11-20 21:53:18 -0500

    updated login login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,67 @@
 import { NavLink } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { collection, addDoc,getDocs,getFirestore} from "firebase/firestore"; 
+import React, {useState } from "react";
+import { collection, query, where, getDocs, getDoc} from "firebase/firestore"; 
 import { db } from './Rtdb.jsx'
 
 
-function  Login() {
-
-  // state variable to pass between
+function Login() {
+  const [isLoading, setIsLoading] = useState(false)
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userName, setUsername] = useState(false)
+  const [password, setPassword] = useState(false)
 
-  try {
-    const docRef = addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
+
+  async function handleClick() {
+    setIsLoading(true);
+    const users = collection(db, "users");
+    const q = query(users, where("username", "==", "admin"), where("password", "==", "admin"));
+    const querySnapshot = await getDocs(q);
+
+    console.log(querySnapshot.size);
+
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.data()['username'], " => ",  doc.data()['password'],);
     });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+
+    setIsLoading(false);
+    
   }
 
-  return (
-        <div class="vh-100 gradient-custom container-fluid vh-80 py-1">
-          <div class="row d-flex justify-content-center align-items-center vh-80">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card shadow-2-strong" style={{borderRadius: "1rem"}}>
-                <div class="card-body p-5 text-center">
-                  <h3 class="mb-5">Sign in</h3>
-                  <div class="form-outline mb-4">
-                    <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                    <label class="form-label" for="typeEmailX-2">Email</label>
-                  </div>
+    return (
+          <div class="vh-100 gradient-custom container-fluid vh-80 py-1">
+            <div class="row d-flex justify-content-center align-items-center vh-80">
+              <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card shadow-2-strong" style={{borderRadius: "1rem"}}>
+                  <div class="card-body p-5 text-center">
+                    <h3 class="mb-5">Sign in</h3>
+                    <div class="form-outline mb-4">
+                      <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+                      <label class="form-label" for="typeEmailX-2">Email</label>
+                    </div>
 
-                  <div class="form-outline mb-4">
-                    <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                    <label class="form-label" for="typePasswordX-2">Password</label>
-                  </div>
+                    <div class="form-outline mb-4">
+                      <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                      <label class="form-label" for="typePasswordX-2">Password</label>
+                    </div>
 
-                  <div class="form-check d-flex justify-content-start mb-4">
-                    <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
-                    <label class="form-check-label" for="form1Example3"> Remember password </label>
+                    <button class="btn btn-primary btn-lg btn-block" onClick={handleClick}>Login</button>
+                    <div>
+                      <ul className="navbar-nav ml-auto">
+                      </ul>
+                    </div>
+                    <hr class="my-4"></hr>
+                    <p>or Register with</p>
+                    <NavLink className="nav-link" to="/CreateAccount">
+                    <button class="btn btn-lg btn-block btn-primary" style={{backgroundColor: "#dd4b39"}}
+                      type="submit"><i class="fab fa-google me-2"></i> Register</button>
+                    </NavLink>
                   </div>
-
-                  <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-                  <div>
-                    <ul className="navbar-nav ml-auto">
-                    </ul>
-                  </div>
-                  <hr class="my-4"></hr>
-                  <p>or Register with</p>
-                  <NavLink className="nav-link" to="/CreateAccount">
-                  <button class="btn btn-lg btn-block btn-primary" style={{backgroundColor: "#dd4b39"}}
-                    type="submit"><i class="fab fa-google me-2"></i> Register</button>
-                  </NavLink>
                 </div>
               </div>
             </div>
-          </div>
-        </div> 
-  );
-}
+          </div> 
+    );
+  }
 
 export default Login;
