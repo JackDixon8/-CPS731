ef92f3bae537067bd66ae8605a26d0c5710e6677
JackDixon8 <Jackdeedixon@gmail.com>
2022-11-28 17:26:01 -0500

    kljhg

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
-import Map from '@urbica/react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
+import Map, {useMap} from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css'
+import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import { render } from '@testing-library/react';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+import * as turf from '@turf/turf'
+import polyline from "@arcgis/core/geometry/Polyline";
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiamFja2RpeG9uOCIsImEiOiJjbGFhNm04eDQwMjN2M29tcW4xdTc2bTFtIn0.C_7t9rnmqC7AnHklldLmaQ';
 export default function Test() {
-  const [viewport, setViewport] = useState({
+ 
+return (
+  
+  <Map
+  initialViewState={{
     latitude: 43.6532,
     longitude: -79.3832,
-    zoom: 11
-  });
-  return(
-  <Map
-    style={{ width: '40%', height: '40vh' }}
-    mapStyle='mapbox://styles/mapbox/light-v9'
-    accessToken={'pk.eyJ1IjoiamFja2RpeG9uOCIsImEiOiJjbGFhNm04eDQwMjN2M29tcW4xdTc2bTFtIn0.C_7t9rnmqC7AnHklldLmaQ'}
-    latitude={viewport.latitude}
-    longitude={viewport.longitude}
-    zoom={viewport.zoom}
-    onViewportChange={setViewport}
-  />
+    zoom: 12
+  }}
+  style={{width: 800, height: 600}}
+  mapStyle="mapbox://styles/mapbox/streets-v9"
+  mapboxAccessToken={MAPBOX_TOKEN}
+>
+ 
 
-  );
-}
+  </Map>
+    
+    
   
+  );
+  
+}
